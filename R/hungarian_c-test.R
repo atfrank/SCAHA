9af073d8c39fd3e52840a27237f0133492595ed3
William <wlfuh@umich.edu>
2017-11-02 17:03:59 -0400

    support for double, hungarian.R

--- R/hungarian_c-test.R
+++ R/hungarian_c-test.R
@@ -47,37 +47,4 @@ test_hungarian <- function(){
   print(get_cost(b))
 }
 
-hungarian <-
-  function(x, maximum = FALSE)
-  {
-    if(!is.matrix(x) || any(x < 0))
-      stop("x must be a matrix with nonnegative entries.")
-    
-    nr <- nrow(x)
-    nc <- ncol(x)
-    if(nr > nc)
-      stop("x must not have more rows than columns.")
-    if(nc > nr)
-      x <- rbind(x, matrix(2 * sum(x), nc - nr, nc))
-    
-    if(maximum) x <- max(x) - x
-    
-    #TODO better support of double
-    # idea: multiply all values by 1000 (3 decimal places), go back to int
-    out <- .C("hungarian", n=as.integer(nc), x=as.integer(round(c(t(x)) * 1000)), p=integer(nc))$p + 1
-    out <- out[seq_len(nr)]
-    class(out) <- "hungarian"
-    out
-  }
-
-print.hungarian <-
-  function(x, ...)
-  {
-    writeLines(c("Optimal assignment:",
-                 gsub("x", " ",
-                      strwrap(paste(seq_along(x), x,
-                                    sep = "x=>x", collapse = ", ")))))
-    invisible(x)
-  }
-
 test_hungarian()
