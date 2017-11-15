340ad2a99a913b3023ed2ae7b05cdc002cd6f7a9
William <wlfuh@umich.edu>
2017-11-15 13:09:55 -0500

    test

--- R/hungarian.c
+++ R/hungarian.c
@@ -6,11 +6,7 @@ void hungarian(int *nin, int *x, Sint *p)
 	n = nin[0];
   cost = x;
   int j;
-  // for(j = 0; j < 100; j++)
-  //   printf("%d ", x[j]);
-  // printf("%s\n", "");
-  int a = run();
-//  printf("%d\n", a);
+  run();
   int i;
   for(i = 0; i < n; i++)
 	 p[i] = xy[i];
