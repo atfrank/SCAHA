d1561b6725970ba649d623d1b4c7873e483893c0
Aaron Frank <afrankz@umich.edu>
2017-11-17 13:58:58 -0500

    added data for testing 2D assignment...

---
+++ R/hungarian.cpp
@@ -0,0 +1,17 @@
+#include <R.h>
+#include "hungarian.h"
+
+void hungarian(int *nin, int *x, Sint *p)
+{
+	n = nin[0];
+  cost = x;
+  int j;
+  // for(j = 0; j < 100; j++)
+  //   printf("%d ", x[j]);
+  // printf("%s\n", "");
+  int a = run();
+//  printf("%d\n", a);
+  int i;
+  for(i = 0; i < n; i++)
+	 p[i] = xy[i];
+}
