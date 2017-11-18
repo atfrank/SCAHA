44eaf330d57d8995e848d50ce147b5bac3197123
Aaron Frank <afrankz@umich.edu>
2017-11-17 20:49:09 -0500

    still figuring out how to weight "distance" calculations

--- R/2D_test.R
+++ R/2D_test.R
@@ -14,7 +14,7 @@ colnames(assgined_computed_cs) <- names
 
 # load unassigned chemical shift data
 unassgined_data <- read.table(file = "tests/region5_2D_hmqc.tab", header = TRUE)
-unassgined_data <- unassgined_data[unassgined_data$volume>0 & unassgined_data$volume<11, ]
+unassgined_data <- unassgined_data[unassgined_data$volume>0, ]
 unassgined_data <- duplicate_unassigned_data(unassgined_data, assgined_computed_cs)
 
 costmat <- matrix(0, nrow = nrow(unassgined_data), ncol = nrow(assgined_computed_cs))
@@ -22,8 +22,9 @@ for (i in 1:nrow(unassgined_data)){
   d1 <- matrix(as.vector(unlist(unassgined_data[i, c("ppm1", "ppm2")])), ncol=2, nrow=nrow(assgined_computed_cs), byrow = TRUE)
   d2 <- matrix(as.vector(unlist(assgined_computed_cs[, c("peak_H", "peak_C")])), ncol=2, byrow = FALSE)
   w2 <- matrix(as.vector(unlist(assgined_computed_cs[, c("weight_H", "weight_C")])), ncol=2, byrow = FALSE)
+  w1 <- unassgined_data$volume[i]*unassgined_data$intensity[i]
   #costmat[i, ] <- rowSums(w2*w2*(d1-d2)*(d1-d2))
-  costmat[i, ] <- w2[,1]*abs(d1[,1]-d2[,1])+w2[,2]*abs(d1[,2]-d2[,2])
+  costmat[i, ] <- (1/w1)*(w2[,1]*abs(d1[,1]-d2[,1])+w2[,2]*abs(d1[,2]-d2[,2]))
   #costmat[i, ] <- abs(d1[,1]-d2[,1])+abs(d1[,2]-d2[,2])
 }
 
